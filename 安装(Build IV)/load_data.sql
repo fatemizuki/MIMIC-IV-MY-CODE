97f5306dcf98a7023e010c2e934fcdb959b93cb7
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-08-22 10:29:16 +0800

    Update load_data.sql

--- 安装(Build IV)/load_data.sql
+++ 安装(Build IV)/load_data.sql
@@ -1,181 +1,169 @@
 -- -------------------------------------------------------------------------------
 --
--- Load data into the MIMIC-III schema
+-- Load data into the MIMIC-IV
+-- Adapted from https://github.com/MIT-LCP/mimic-code/blob/master/buildmimic/postgres/postgres_load_data.sql
 --
 -- -------------------------------------------------------------------------------
 
---------------------------------------------------------
---  File created - Thursday-August-27-2015
---------------------------------------------------------
-
--- Change to the directory containing the data files
-\cd :mimic_data_dir
-
--- If running scripts individually, you can set the schema where all tables are created as follows:
--- SET search_path TO mimiciii;
-
--- Restoring the search path to its default value can be accomplished as follows:
--- SET search_path TO "$user",public;
-
-/* Set the mimic_data_dir variable to point to directory containing
-   all .csv files. If using Docker, this should not be changed here.
-   Rather, when running the docker container, use the -v option
-   to have Docker mount a host volume to the container path /mimic_data
-   as explained in the README file
-*/
 
 --------------------------------------------------------
 --  Load Data for Table ADMISSIONS
 --------------------------------------------------------
 
-\copy ADMISSIONS FROM 'ADMISSIONS.csv' DELIMITER ',' CSV HEADER NULL ''
-
---------------------------------------------------------
---  Load Data for Table CALLOUT
---------------------------------------------------------
-
-\copy CALLOUT from 'CALLOUT.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table CAREGIVERS
---------------------------------------------------------
-
-\copy CAREGIVERS from 'CAREGIVERS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table CHARTEVENTS
---------------------------------------------------------
-
-\copy CHARTEVENTS from 'CHARTEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table CPTEVENTS
---------------------------------------------------------
-
-\copy CPTEVENTS from 'CPTEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table DATETIMEEVENTS
---------------------------------------------------------
-
-\copy DATETIMEEVENTS from 'DATETIMEEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table DIAGNOSES_ICD
---------------------------------------------------------
-
-\copy DIAGNOSES_ICD from 'DIAGNOSES_ICD.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table DRGCODES
---------------------------------------------------------
-
-\copy DRGCODES from 'DRGCODES.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table D_CPT
---------------------------------------------------------
-
-\copy D_CPT from 'D_CPT.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table D_ICD_DIAGNOSES
---------------------------------------------------------
-
-\copy D_ICD_DIAGNOSES from 'D_ICD_DIAGNOSES.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table D_ICD_PROCEDURES
---------------------------------------------------------
-
-\copy D_ICD_PROCEDURES from 'D_ICD_PROCEDURES.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table D_ITEMS
---------------------------------------------------------
-
-\copy D_ITEMS from 'D_ITEMS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table D_LABITEMS
---------------------------------------------------------
-
-\copy D_LABITEMS from 'D_LABITEMS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table ICUSTAYS
---------------------------------------------------------
-
-\copy ICUSTAYS from 'ICUSTAYS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table INPUTEVENTS_CV
---------------------------------------------------------
-
-\copy INPUTEVENTS_CV from 'INPUTEVENTS_CV.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table INPUTEVENTS_MV
---------------------------------------------------------
-
-\copy INPUTEVENTS_MV from 'INPUTEVENTS_MV.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table LABEVENTS
---------------------------------------------------------
-
-\copy LABEVENTS from 'LABEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table MICROBIOLOGYEVENTS
---------------------------------------------------------
-
-\copy MICROBIOLOGYEVENTS from 'MICROBIOLOGYEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table NOTEEVENTS
---------------------------------------------------------
-
-\copy NOTEEVENTS from 'NOTEEVENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table OUTPUTEVENTS
---------------------------------------------------------
-
-\copy OUTPUTEVENTS from 'OUTPUTEVENTS.csv' delimiter ',' csv header NULL ''
+\copy ADMISSIONS FROM 'admissions.csv' DELIMITER ',' CSV HEADER NULL ''
 
 --------------------------------------------------------
 --  Load Data for Table PATIENTS
 --------------------------------------------------------
 
-\copy PATIENTS from 'PATIENTS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table PRESCRIPTIONS
---------------------------------------------------------
-
-\copy PRESCRIPTIONS from 'PRESCRIPTIONS.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table PROCEDUREEVENTS_MV
---------------------------------------------------------
-
-\copy PROCEDUREEVENTS_MV from 'PROCEDUREEVENTS_MV.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table PROCEDURES_ICD
---------------------------------------------------------
-
-\copy PROCEDURES_ICD from 'PROCEDURES_ICD.csv' delimiter ',' csv header NULL ''
-
---------------------------------------------------------
---  Load Data for Table SERVICES
---------------------------------------------------------
-
-\copy SERVICES from 'SERVICES.csv' delimiter ',' csv header NULL ''
+\copy PATIENTS from 'patients.csv' delimiter ',' csv header NULL ''
 
 --------------------------------------------------------
 --  Load Data for Table TRANSFERS
 --------------------------------------------------------
 
-\copy TRANSFERS from 'TRANSFERS.csv' delimiter ',' csv header NULL ''
+\copy TRANSFERS from 'transfers.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_HCPCS
+--------------------------------------------------------
+
+\copy D_HCPCS from 'd_hcpcs.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ICD_DIAGNOSES
+--------------------------------------------------------
+
+\copy D_ICD_DIAGNOSES from 'd_icd_diagnoses.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ICD_PROCEDURES
+--------------------------------------------------------
+
+\copy  D_ICD_PROCEDURES from 'd_icd_procedures.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_LABITEMS
+--------------------------------------------------------
+
+\copy  D_LABITEMS from 'd_labitems.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DIAGNOSES_ICD
+--------------------------------------------------------
+
+\copy DIAGNOSES_ICD from 'diagnoses_icd.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DRGCODES
+--------------------------------------------------------
+
+\copy DRGCODES from 'drgcodes.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table EMAR_DETAIL
+--------------------------------------------------------
+
+\copy EMAR_DETAIL from 'emar_detail.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table EMAR
+--------------------------------------------------------
+
+\copy EMAR from 'emar.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table HCPCSEVENTS
+--------------------------------------------------------
+
+\copy HCPCSEVENTS from 'hcpcsevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table LABEVENTS
+--------------------------------------------------------
+
+\copy LABEVENTS from 'labevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table MICROBIOLOGYEVENTS
+--------------------------------------------------------
+
+\copy MICROBIOLOGYEVENTS from 'microbiologyevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PHARMACY
+--------------------------------------------------------
+
+\copy PHARMACY from 'pharmacy.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table POE_DETAIL
+--------------------------------------------------------
+
+\copy POE_DETAIL from 'poe_detail.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table POE
+--------------------------------------------------------
+
+\copy POE from 'poe.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PRESCRIPTIONS
+--------------------------------------------------------
+
+\copy PRESCRIPTIONS from 'prescriptions.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PROCEDURES_ICD
+--------------------------------------------------------
+
+\copy PROCEDURES_ICD from 'procedures_icd.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table SERVICES
+--------------------------------------------------------
+
+\copy SERVICES from 'services.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table INPUTEVENTS
+--------------------------------------------------------
+
+\copy INPUTEVENTS from 'inputevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table OUTPUTEVENTS
+--------------------------------------------------------
+
+\copy OUTPUTEVENTS from 'outputevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table DATETIMEEVENTS
+--------------------------------------------------------
+
+\copy DATETIMEEVENTS from 'datetimeevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table ICUSTAYS
+--------------------------------------------------------
+
+\copy ICUSTAYS from 'icustays.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table CHARTEVENTS
+--------------------------------------------------------
+
+\copy CHARTEVENTS from 'chartevents.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table D_ITEMS
+--------------------------------------------------------
+
+\copy D_ITEMS from 'd_items.csv' delimiter ',' csv header NULL ''
+
+--------------------------------------------------------
+--  Load Data for Table PROCEDUREEVENTS
+--------------------------------------------------------
+
+\copy PROCEDUREEVENTS from 'PROCEDUREEVENTS.csv' delimiter ',' csv header NULL ''
