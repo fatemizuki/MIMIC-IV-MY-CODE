4326ea6ebd0f37626c850431676c366c5db33c00
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 16:41:43 +0800

    Create prescriptions.sql

---
+++ 探索(Explore)/prescriptions.sql
@@ -0,0 +1,10 @@
+-- prescriptions探索
+
+-- 1. drug_type
+-- SELECT drug_type FROM prescriptions
+-- GROUP BY drug_type
+
+-- 2. drug and gsn
+-- SELECT drug, gsn FROM prescriptions
+-- GROUP BY drug, gsn
+-- ORDER BY drug, gsn
