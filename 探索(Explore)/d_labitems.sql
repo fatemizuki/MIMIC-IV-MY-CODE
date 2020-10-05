eb68417ebb779eaa7067eab70db42d2191f13f93
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 09:21:56 +0800

    Create d_labitems.sql

---
+++ 探索(Explore)/d_labitems.sql
@@ -0,0 +1,18 @@
+-- d_labitems探索
+
+-- 1. 总览
+-- SELECT * FROM d_labitems
+-- LIMIT 100
+
+-- 2. 组织液分类
+-- SELECT fluid, COUNT(*) FROM d_labitems
+-- GROUP BY fluid
+
+-- 3. 化验种类分类
+-- SELECT category, COUNT(*) FROM d_labitems
+-- GROUP BY category
+
+-- 4. 总和
+-- SELECT fluid, category, COUNT(*) FROM d_labitems
+-- GROUP BY fluid, category 
+-- ORDER BY fluid, category
