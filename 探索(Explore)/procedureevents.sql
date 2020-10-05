f4e2c1787a61a8fcdf8f25a2975aab02993d69c7
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 16:57:40 +0800

    Create procedureevents.sql

---
+++ 探索(Explore)/procedureevents.sql
@@ -0,0 +1,13 @@
+-- procedureevents探索
+
+--1. ordercategoryname
+-- SELECT ordercategoryname FROM procedureevents
+-- GROUP BY ordercategoryname
+
+-- 2. ordercategorydescription
+-- SELECT ordercategorydescription FROM procedureevents
+-- GROUP BY ordercategorydescription
+
+-- 3. statusdescroption
+-- SELECT statusdescroption FROM procedureevents
+-- GROUP BY statusdescroption
