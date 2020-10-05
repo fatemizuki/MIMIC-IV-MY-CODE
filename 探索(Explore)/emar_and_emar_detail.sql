e5480d6bfda8098fee54a76ecc03a88a9cab3df2
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 10:59:57 +0800

    Create emar_and_emar_detail.sql

---
+++ 探索(Explore)/emar_and_emar_detail.sql
@@ -0,0 +1,21 @@
+-- emar/emar_detail探索
+
+-- 1. medication种类
+-- SELECT medication, COUNT(*) FROM emar
+-- GROUP BY medication
+-- ORDER BY medication
+
+-- 2. event_txt种类
+-- SELECT event_txt, COUNT(*) FROM emar
+-- GROUP BY event_txt
+-- ORDER BY event_txt
+
+-- 3. administration_types种类
+-- SELECT administration_types, COUNT(*) FROM emar_detail
+-- GROUP BY administration_types
+-- ORDER BY administration_types
+
+-- 4. barcode_type种类
+-- SELECT barcode_type, COUNT(*) FROM emar_detail
+-- GROUP BY barcode_type
+-- ORDER BY barcode_type
