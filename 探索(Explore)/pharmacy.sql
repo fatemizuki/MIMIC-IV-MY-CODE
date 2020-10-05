683ce3c4528f674cfa7d7275490ed515564c8073
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 11:37:00 +0800

    Create pharmacy.sql

---
+++ 探索(Explore)/pharmacy.sql
@@ -0,0 +1,21 @@
+-- pharmacy探索
+
+-- 1.药名
+-- SELECT medication FROM pharmacy
+-- GROUP BY medication
+
+-- 2. proc_type
+-- SELECT proc_type, COUNT(*) FROM pharmacy
+-- GROUP BY proc_type
+
+-- 3. status
+-- SELECT status, COUNT(*) FROM pharmacy
+-- GROUP BY status
+
+-- 4. route
+-- SELECT route, COUNT(*) FROM pharmacy
+-- GROUP BY route
+
+-- 5. frequency
+-- SELECT frequency, COUNT(*) FROM pharmacy
+-- GROUP BY frequency
