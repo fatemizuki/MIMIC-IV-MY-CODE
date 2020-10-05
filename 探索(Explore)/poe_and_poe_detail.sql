29110fd1508d3d4f88ce73660db4b75db472624b
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 16:31:24 +0800

    Create poe_and_poe_detail.sql

---
+++ 探索(Explore)/poe_and_poe_detail.sql
@@ -0,0 +1,21 @@
+-- poe/poe_detail探索
+
+-- 1. order_type
+-- SELECT order_type,COUNT(*) FROM poe
+-- GROUP BY order_type
+
+-- 2. order_subtype
+-- SELECT order_subtype, COUNT(*) FROM poe
+-- GROUP BY order_subtype
+
+-- 3. transaction_type
+-- SELECT transaction_type, COUNT(*) FROM poe
+-- GROUP BY transaction_type
+
+-- 4. field_name
+-- SELECT field_name, COUNT(*) FROM poe_detail
+-- GROUP BY field_name
+
+-- 5. field_value
+-- SELECT field_value, COUNT(*) FROM poe_detail
+-- GROUP BY field_value
