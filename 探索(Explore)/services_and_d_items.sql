7b92a06c64352bc38e3a63dd1a9c80194100d9ea
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-10-05 16:46:12 +0800

    Create services_and_d_items.sql

---
+++ 探索(Explore)/services_and_d_items.sql
@@ -0,0 +1,11 @@
+-- services探索
+
+-- 1. curr_service
+-- SELECT curr_service, COUNT(*) FROM services
+-- GROUP BY curr_service
+
+-- d_items探索
+
+-- 1. linksto
+-- SELECT linksto, COUNT(*) FROM d_items
+-- GROUP BY linksto
